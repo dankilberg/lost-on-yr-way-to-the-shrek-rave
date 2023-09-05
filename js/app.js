71a01dbd9cfaca9a4c35c566ff7722fd2f31fdae
User Namedankilbergkilbd910@gmail.comgit config --global user.email user@email.comkilbd910@gmail.com <kilbd910@gmail.com>
2023-09-05 15:48:09 -0400

    Add style to elements

--- js/app.js
+++ js/app.js
@@ -3,14 +3,18 @@
 /*---------------------------- Variables (state) ----------------------------*/
  let start, game, end
 /*------------------------ Cached Element References ------------------------*/
-const startButton = document.querySelector('start-button')
+const startButton = document.querySelector('#start-button')
+const containerOne = document.querySelector('#text')
+const optionButtonsEl = document.querySelector
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 // document.addEventListener('button')
-
+startButton.addEventListener ('click', startGame)
 
 /*-------------------------------- Functions --------------------------------*/
-function startGame(){
+function startGame(){ 
+  const startButton =  
+  textE
 
 }
