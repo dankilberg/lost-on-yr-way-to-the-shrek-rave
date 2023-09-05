44ca872c66347e9885016953e740d00b520905b4
User Namedankilbergkilbd910@gmail.comgit config --global user.email user@email.comkilbd910@gmail.com <kilbd910@gmail.com>
2023-09-05 14:26:54 -0400

    Update Start Button

--- js/app.js
+++ js/app.js
@@ -3,9 +3,10 @@
 /*---------------------------- Variables (state) ----------------------------*/
  let start, game, end
 /*------------------------ Cached Element References ------------------------*/
-
+const startButton = document.qu
 
 /*----------------------------- Event Listeners -----------------------------*/
+// document.addEventListener('button')
 
 
 /*-------------------------------- Functions --------------------------------*/
