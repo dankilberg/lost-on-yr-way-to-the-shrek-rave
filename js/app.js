287c4ee36054d0171f69729b282d8cef37eb4f37
User Namedankilbergkilbd910@gmail.comgit config --global user.email user@email.comkilbd910@gmail.com <kilbd910@gmail.com>
2023-09-05 16:11:23 -0400

    function startGame test

--- js/app.js
+++ js/app.js
@@ -4,17 +4,21 @@
  let start, game, end
 /*------------------------ Cached Element References ------------------------*/
 const startButton = document.querySelector('#start-button')
-const containerOne = document.querySelector('#text')
+const textEl = document.querySelector('#text')
 const optionButtonsEl = document.querySelector('#option-buttons')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 // document.addEventListener('button')
-startButton.addEventListener ('click', startGame)
+startButton.addEventListener('click', startGame)
 
 /*-------------------------------- Functions --------------------------------*/
 function startGame(){ 
-  const startButton =  
-  textE
-
+  textEl.style.display = 'block'
+  optionButtonsEl.style.display = 'block'
+  textEl.innerHTML = "Welcome to 'Lost on Yr Way To The Shrek Rave.'"
+  optionButtonsEl.innerHTML = 
+  `<button class="btn">Option 1</button>
+  <button class="btn">Option 2</button>
+  <button class="btn">Option 3</button>`
 }
