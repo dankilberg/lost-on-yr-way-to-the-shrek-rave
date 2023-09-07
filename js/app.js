224bbb6684a6210441f2ec7ccbc3d8e848859ed9
User Namedankilbergkilbd910@gmail.comgit config --global user.email user@email.comkilbd910@gmail.com <kilbd910@gmail.com>
2023-09-06 22:37:34 -0400

    Add Module Date

--- js/app.js
+++ js/app.js
@@ -1,66 +1,12 @@
 /*-------------------------------- Constants --------------------------------*/
-const story = [
-  {
-    prompt: "The Shrek rave starts in an hour and even though you already kind of look like Shrek you are nowhere near being Shrek. What do you do?",
-    options: [
-      {
-        prompt: "Index 1",
-        nextIndex: 1
-      },
-      {
-        prompt: "Index 2",
-        nextIndex: 2
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 2",
-    options: [
-      {
-        prompt: "Index 3",
-        nextIndex: 3
-      },
-      {
-        prompt: "Index 4",
-        nextIndex: 4
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 3",
-    options: [
-      {
-        prompt: "Index4",
-        nextIndex: 5
-      },
-      {
-        prompt: "Index 6",
-        nextIndex: 6
-      }
-      
-    ]
-  },
-  {
-    prompt: "Prompt 4",
-    options: [
-      {
-        prompt: "Index 7",
-        nextIndex: 7
-      },
-      {
-        prompt: "Index 8",
-        nextIndex: 8
-      }
-    ]
-  },
-]
+import { story } from "./shrek.js"
 /*---------------------------- Variables (state) ----------------------------*/
  let currentIndex = 0
 /*------------------------ Cached Element References ------------------------*/
 const startButton = document.querySelector('#start-button')
 const textEl = document.querySelector('#text')
 const optionButtonsEl = document.querySelector('#option-buttons')
-const button = document.createElement('button')
+//const button = document.createElement('button')
 const title = document.querySelector('#title1')
 // const classBtn = ocument.querySelector('btn')
  
@@ -70,6 +16,7 @@ const title = document.querySelector('#title1')
 startButton.addEventListener('click', startGame)
 optionButtonsEl.addEventListener.apply('click', select)
 
+
 /*-------------------------------- Functions --------------------------------*/
 function startGame(){ 
   startButton.style.display = 'none'
@@ -78,20 +25,21 @@ function startGame(){
 } 
 
 function showText() {
-  const currentState = story[currentIndex];
-  textEl.style.display = 'block';
-  textEl.innerHTML = currentState.prompt;
+  const currentState = story[currentIndex]
+  textEl.style.display = 'block'
+  textEl.innerHTML = currentState.prompt
 
-  optionButtonsEl.style.display = 'block';
-  optionButtonsEl.innerHTML = '';
+  optionButtonsEl.style.display = 'block'
+  optionButtonsEl.innerHTML = ''
 
   currentState.options.forEach((option, index) => {
-    const button = document.createElement('button');
-    button.classList.add('btn');
-    button.textContent = option.prompt;
-    button.dataset.index = index;
-    optionButtonsEl.appendChild(button);
-  });
+    const button = document.createElement('button')
+    button.classList.add('btn')
+    button.textContent = option.prompt
+    button.dataset.index = index
+    button.addEventListener('click', select)
+    optionButtonsEl.appendChild(button)
+  })
 }
 
 
@@ -104,8 +52,15 @@ function select(event) {
     currentIndex = selectOption.nextIndex
     showText()
   }
-
 }
+
+
+
+
+
+
+
+
 //too many names for button? go back and check everything tide to button
 //event
 //click?
---
+++ js/shrek.js
@@ -0,0 +1,564 @@
+const story = [
+  {
+    prompt: "The Shrek rave starts in an hour and even though you already kind of look like Shrek you are nowhere near being Shrek. What do you do?",
+    options: [
+      {
+        prompt: "Green Paint",
+        nextIndex: 1
+      },
+      {
+        prompt: "Red Paint",
+        nextIndex: 2
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 2",
+    options: [
+      {
+        prompt: "Index 3",
+        nextIndex: 3
+      },
+      {
+        prompt: "Index 4",
+        nextIndex: 4
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 3",
+    options: [
+      {
+        prompt: "Index4",
+        nextIndex: 5
+      },
+      {
+        prompt: "Index 6",
+        nextIndex: 6
+      }
+      
+    ]
+  },
+  {
+    prompt: "Prompt 4",
+    options: [
+      {
+        prompt: "Index 7",
+        nextIndex: 7
+      },
+      {
+        prompt: "Index 8",
+        nextIndex: 8
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 9
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 10
+      }
+    ]
+  },
+  
+]
+
+
+
+
+
+
+
+
+
+
+
+export {
+  story
+}
