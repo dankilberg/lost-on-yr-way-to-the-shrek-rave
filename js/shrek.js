dab82b080364974d91c981b42c885639652fba0c
User Namedankilbergkilbd910@gmail.comgit config --global user.email user@email.comkilbd910@gmail.com <kilbd910@gmail.com>
2023-09-07 01:54:26 -0400

    Copy and Prompt layout

--- js/shrek.js
+++ js/shrek.js
@@ -1,46 +1,54 @@
 const story = [
   {
-    prompt: "The Shrek rave starts in an hour and even though you already kind of look like Shrek you are nowhere near being Shrek. What do you do?",
+    prompt: "The Shrek rave starts in an hour, and even though you already kind of look like Shrek, you are nowhere near being Shrek. What do you do?",
     options: [
       {
-        prompt: "Green Paint",
+        prompt: "Slather on the Green Paint",
         nextIndex: 1
       },
       {
-        prompt: "Red Paint",
+        prompt: "Watch the movie Shrek",
         nextIndex: 2
       }
     ]
   },
   {
-    prompt: "Prompt 2",
+    prompt: "You have slathered on the green paint. The smell is different, more chemically than you remember. You read the label. It says, 'If even a drop of paint gets on your skin, head to the emergency room immediately. One drop can be fatal'",
     options: [
       {
-        prompt: "Index 3",
+        prompt: "Head to the emergancy room",
         nextIndex: 3
       },
       {
-        prompt: "Index 4",
+        prompt: "Head to the rave",
         nextIndex: 4
       }
     ]
   },
   {
-    prompt: "Prompt 3",
+    prompt: "You have aleady seen the movie Shrek. Many times.",
     options: [
       {
-        prompt: "Index4",
-        nextIndex: 5
-      },
-      {
-        prompt: "Index 6",
-        nextIndex: 6
-      }
-      
+        prompt: "Return",
+        nextIndex: 0
+      },  
     ]
   },
   {
-    prompt: "Prompt 4",
+    prompt: "When you walk into the emergency room, you notice fellow Shreks have made the same mistake. The toxic smell has grown stronger and you have become dizzy. Some have broke character and are pretty friendly. They went to make conversation with you.",
+    options: [
+      {
+        prompt: "Ignore them. You're Shrek. You're an Ogrer, a loner, a rebel.",
+        nextIndex: 5
+      },
+      {
+        prompt: "In your heart of hearts, you know you're not really Shrek. You can talk to another person who has the same interests as you.",
+        nextIndex: 6
+      }
+    ]
+  },
+  {
+    prompt: "Prompt 5",
     options: [
       {
         prompt: "Index 7",
@@ -53,7 +61,7 @@ const story = [
     ]
   },
   {
-    prompt: "Prompt 5",
+    prompt: "Prompt 6",
     options: [
       {
         prompt: "Index 9",
@@ -62,515 +70,181 @@ const story = [
       {
         prompt: "Index 10",
         nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
-  },
-  {
-    prompt: "Prompt 5",
-    options: [
-      {
-        prompt: "Index 9",
-        nextIndex: 9
-      },
-      {
-        prompt: "Index 10",
-        nextIndex: 10
-      }
-    ]
+      } 
+    ]  
   },
 
+  {
+    prompt: "Prompt 7",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 11
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 12
+      } 
+    ]  
+  },
+
+  {
+    prompt: "Prompt 8",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 13
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 14
+      } 
+    ]   
+  },
+  {
+    prompt: "Prompt 9",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 15
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 16
+      } 
+    ]   
+  },
+  {
+    prompt: "Prompt 10",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 17
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 18
+      } 
+    ]   
+  },
+  {
+    prompt: "Prompt 11",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 19
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 20
+      } 
+    ]   
+  },
+  {
+    prompt: "Prompt 12",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 21
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 22
+      } 
+    ]   
+  },
+  {
+    prompt: "Prompt 13",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 23
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 24
+      } 
+    ]   
+  },
+  {
+    prompt: "Prompt 14",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 25
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 26
+      } 
+    ]   
+  },
+  {
+    prompt: "Prompt 15",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 27
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 28
+      } 
+    ]   
+  },
+  {
+    prompt: "Prompt 16",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 29
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 30
+      } 
+    ]   
+  },
+  {
+    prompt: "Prompt 17",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 31
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 32
+      } 
+    ]   
+  },
+  {
+    prompt: "Prompt 18",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 33
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 34
+      } 
+    ]   
+  },
+  {
+    prompt: "Prompt 19",
+    options: [
+      {
+        prompt: "Index 9",
+        nextIndex: 35
+      },
+      {
+        prompt: "Index 10",
+        nextIndex: 36
+      } 
+    ]   
+  },
 ]
-
-
-
-
-
-
-
-
-
-
 
 export {
   story
